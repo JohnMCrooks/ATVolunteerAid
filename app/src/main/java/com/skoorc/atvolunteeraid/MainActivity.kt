d913d3bd36f9bb08df463f3e298af9293c9185d1
Crooks5001 <Crooks5001@gmail.com>
2020-11-01 17:43:49 -0500

    Have GPS location marking on button click. TODO: add sqllite DB, DAO class, create viewModel, new view for viewing list of reported markers

--- app/src/main/java/com/skoorc/atvolunteeraid/MainActivity.kt
+++ app/src/main/java/com/skoorc/atvolunteeraid/MainActivity.kt
@@ -1,137 +1,18 @@
 package com.skoorc.atvolunteeraid
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.pm.PackageManager
-import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import androidx.core.app.ActivityCompat
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.material.snackbar.Snackbar
-import com.skoorc.atvolunteeraid.util.checkSelfPermissionCompat
-import com.skoorc.atvolunteeraid.util.requestPermissionsCompat
-import com.skoorc.atvolunteeraid.util.shouldShowRequestPermissionRationaleCompat
-import com.skoorc.atvolunteeraid.util.showSnackbar
 
-const val PERMISSION_REQUEST_FINE_LOCATION = 0
-const val PERMISSION_REQUEST_COARSE_LOCATION = 1
 const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsResultCallback {
-    private lateinit var layout: View
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var latitudeText: TextView
-    private lateinit var longitudeText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.i(TAG, "Pre-view inflation")
         setContentView(R.layout.activity_main)
-        layout = findViewById(R.id.overview_fragment)
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        checkGPSPermissions()
         Log.i(TAG, "End of onCreate created")
-
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        Log.i(TAG, "start onRequestPermissionsResult")
-        if (requestCode == PERMISSION_REQUEST_FINE_LOCATION) {
-            // Request for camera permission.
-            if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Log.i(TAG, "Permission was granted for ACCESS_FINE_LOCATION ")
-                // Permission has been granted. Do whatever with the GPS info.
-//                TODO("Record location and log it probably using GPS location service")
-                getLastLocation()
-            } else {
-                // Permission request was denied.)
-                Log.i(TAG, "Permission was Denied for ACCESS_FINE_LOCATION ")
-            }
-        }
-    }
-
-    private fun checkGPSPermissions() {
-        Log.i(TAG, "start checkGPSPermissions ")
-        // Check if the GPS permission has been granted
-        if (checkSelfPermissionCompat(Manifest.permission.ACCESS_FINE_LOCATION) ==
-            PackageManager.PERMISSION_GRANTED) {
-            // Permission is already available, start camera preview
-            Log.i(TAG, "GPS Permissions already exist")
-            getLastLocation()
-        } else {
-            Log.i(TAG, "GPS Permissions don't exist yet - requesting permissions")
-            // Permission is missing and must be requested.
-            requestGPSPermission()
-        }
-    }
-
-    /**
-     * Requests the [android.Manifest.permission.ACCESS_FINE_LOCATION] permission.
-     * If an additional rationale should be displayed, the user has to launch the request from
-     * a SnackBar that includes additional information.
-     */
-    private fun requestGPSPermission() {
-        Log.i(TAG, "Start GPS permissions request")
-        // Permission has not been granted and must be requested.
-        if (shouldShowRequestPermissionRationaleCompat(Manifest.permission.ACCESS_FINE_LOCATION)) {
-            // Provide an additional rationale to the user if the permission was not granted
-            // and the user would benefit from additional context for the use of the permission.
-            // Display a SnackBar with a button to request the missing permission.
-            layout.showSnackbar("GPS access required for the app to work",
-                Snackbar.LENGTH_INDEFINITE, "OK") {
-                requestPermissionsCompat(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                    PERMISSION_REQUEST_FINE_LOCATION)
-            }
-
-        } else {
-            Log.i(TAG, "GPS Access Denied, requesting permission")
-            // Request the permission. The result will be received in onRequestPermissionResult().
-            requestPermissionsCompat(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_REQUEST_FINE_LOCATION)
-        }
-    }
-
-    fun getLastLocation() {
-        Log.i(TAG, "inside getLastLocation")
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            Log.i(TAG, "Either Fine or Coarse were denied, Location won't be found")
-            return
-        } else {
-            Log.i(TAG, "All necessary permissions granted proceeding to get new location")
-//            TODO("Figure out why Locations are coming back with a null value on the emualtor.")
-            fusedLocationClient.lastLocation
-                .addOnCompleteListener { taskLocation ->
-                    Log.i(TAG, "inside location listener")
-                    if (taskLocation.isSuccessful) {
-                        val location = taskLocation.result
-                        latitudeText = findViewById(R.id.LatValue)
-                        longitudeText = findViewById(R.id.longValue)
-                        latitudeText.text = location?.latitude.toString()
-                        Log.i(TAG, "Lat: ${location.latitude}")
-                        Log.i(TAG, "Long: ${location.longitude}")
-                        longitudeText.text = location?.longitude.toString()
-                    } else {
-                        Log.i(TAG, "No location detected")
-                    }
-                }
-        }
     }
 }
--- app/src/main/java/com/skoorc/atvolunteeraid/overview/OverviewFragment.kt
+++ app/src/main/java/com/skoorc/atvolunteeraid/overview/OverviewFragment.kt
@@ -1,20 +1,30 @@
 package com.skoorc.atvolunteeraid.overview
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.material.snackbar.Snackbar
 import com.skoorc.atvolunteeraid.R
-import com.skoorc.atvolunteeraid.util.showSnackbar
 import kotlinx.android.synthetic.main.fragment_overview.view.*
 
 class OverviewFragment: Fragment() {
     private val TAG = "fragmentOverview"
     private lateinit var layout: View
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var latitudeText: TextView
+    private lateinit var longitudeText: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,7 +32,8 @@ class OverviewFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         Log.i(TAG,   "Entering OnCreateView for overview fragment")
-        return  inflater.inflate(R.layout.fragment_overview, container, false)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext() as Context)
+        return   inflater.inflate(R.layout.fragment_overview, container, false)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +44,7 @@ class OverviewFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        layout = view
         Log.i(TAG,   "Entering OnViewCreated for overview fragment")
 
         view.markLocationButton.setOnClickListener {
@@ -50,6 +62,38 @@ class OverviewFragment: Fragment() {
     fun onClickReportLocation(view: View) {
         Log.i(TAG,   "Inside onClickReportLocation")
         Toast.makeText(context, "It's a Doodie", Toast.LENGTH_SHORT).show()
+        getLastLocation()
     }
 
+    fun getLastLocation() {
+        Log.i(com.skoorc.atvolunteeraid.TAG, "inside getLastLocation")
+        if (ActivityCompat.checkSelfPermission(
+                getContext() as Context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                getContext() as Context,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.i(com.skoorc.atvolunteeraid.TAG, "Either Fine or Coarse were denied, Location won't be found")
+            return
+        } else {
+            Log.i(com.skoorc.atvolunteeraid.TAG, "All necessary permissions granted proceeding to get new location")
+            fusedLocationClient.lastLocation
+                .addOnCompleteListener { taskLocation ->
+                    Log.i(com.skoorc.atvolunteeraid.TAG, "inside location listener")
+                    if (taskLocation.isSuccessful) {
+                        val location = taskLocation.result
+                        var listTextView = layout.tempTextView
+                        Log.i(TAG, "Random num to show it's running: ${Math.random()}")
+                        Log.i(com.skoorc.atvolunteeraid.TAG, "Lat: ${location.latitude}")
+                        Log.i(com.skoorc.atvolunteeraid.TAG, "Long: ${location.longitude}")
+                        val textPlaceholder =  "${listTextView.text} \n ${location.latitude}, ${location.longitude}"
+                         listTextView.text = textPlaceholder
+                    } else {
+                        Log.i(com.skoorc.atvolunteeraid.TAG, "No location detected")
+                    }
+                }
+        }
+    }
 }
