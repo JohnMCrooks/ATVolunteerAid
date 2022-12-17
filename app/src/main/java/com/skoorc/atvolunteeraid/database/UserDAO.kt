6f771e49e8028ad994f989ec8a912e856464e006
Crooks5001 <Crooks5001@gmail.com>
2022-12-17 17:10:34 -0500

    Updating files to remove build errors after updating dependencies

--- app/src/main/java/com/skoorc/atvolunteeraid/database/UserDAO.kt
+++ app/src/main/java/com/skoorc/atvolunteeraid/database/UserDAO.kt
@@ -5,7 +5,6 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.skoorc.atvolunteeraid.model.Location
 import com.skoorc.atvolunteeraid.model.User
 
 @Dao
@@ -28,10 +27,10 @@ interface UserDAO {
     @Query ("SELECT * FROM user_table WHERE admin_status='true' ORDER BY date_joined DESC")
     fun getAllAdminUsers(): LiveData<List<User>>
 
-    @Query ("UPDATE user_table SET admin_status=:boolean WHERE id= :userId")
-    fun setAdminStatusById(userId: Int, boolean: Boolean)
+    @Query ("UPDATE user_table SET admin_status= :isAdmin WHERE id= :userId")
+    fun setAdminStatusById(userId: Int, isAdmin: Boolean)
 
-    @Query ("UPDATE user_table SET is_shadow_banned=:boolean WHERE id= :userId")
-    fun setShadowBanStatusById(userId: Int, boolean: Boolean)
+    @Query ("UPDATE user_table SET is_shadow_banned= :isBanned WHERE id= :userId")
+    fun setShadowBanStatusById(userId: Int, isBanned: Boolean)
 
 }
